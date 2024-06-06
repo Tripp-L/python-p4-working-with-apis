e31c4e9c6d527ab1ea2b5f2e677ce9ddae662629
trippl <tlaursen8@yahoo.com>
2024-06-06 10:28:36 -0400

    Working With APIs Code Along

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,24 @@
-
 import requests
-import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        response = requests.get(URL)
+        return response.json()  # This will parse the JSON response into Python data structures
 
-    response = requests.get(URL)
-    return response.content
+    def program_agencies(self):
+        programs_list = []
+        programs = self.get_programs()  # This will already be a list of dictionaries
+        for program in programs:
+            if 'agency' in program:  # Make sure 'agency' key exists in the dictionary
+                programs_list.append(program['agency'])
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+# Example usage:
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
+  
